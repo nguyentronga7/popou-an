7ea7fc41166eb012ee6782bc81b23b7711e85068
nguyentronga7 <ceocaigicungre@gmail.com>
2017-04-10 14:59:31 +0700

    Update popupan-an-go-duoi.js

--- popupan-an-go-duoi.js
+++ popupan-an-go-duoi.js
@@ -1,8 +1,8 @@
 
-	var url_popup = 'http://bit.ly/1JdPEQv';
+	var url_popup = 'http://youcare.vn/';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'http://youcare.vn/';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
